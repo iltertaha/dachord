deb019430660d6c03d7f4974d1e78e0c6ec8d9c1
iltertaha <ilteraktolga@gmail.com>
2021-11-17 22:35:51 +0300

    Implemented Controller

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{   [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
