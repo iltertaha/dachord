23ea0eb763b7e51b425fadbb388a7d238a5ae4d0
iltertaha <ilteraktolga@gmail.com>
2021-12-08 22:58:56 +0300

    Edit Event

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -27,9 +27,19 @@ namespace API.Controllers
         } 
         
         [HttpPost]
-        public async Task<IActionResult> CreateActivit([FromBody]Activity activity)
+        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
         {
             return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity}));
+
+
+
         }
     }
 }
