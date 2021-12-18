1e7a7229219504341033a072f03643eb7e60032f
iltertaha <ilteraktolga@gmail.com>
2021-12-18 16:41:48 +0300

    Extension method for startup services

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 using Application.Core;
 using Application.MusicEvents;
 using MediatR;
@@ -35,25 +36,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-
-            });
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                 {
-                     // allow any post get etc header coming from the frontend
-                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                 });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationServices(_config);
 
 
         }
