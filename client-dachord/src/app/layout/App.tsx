9297878a9c36eb7069b86374884a642fefa8eb6f
iltertaha <ilteraktolga@gmail.com>
2022-03-17 22:13:53 +0300

    Separate home page, dashboard and form

--- client-dachord/src/app/layout/App.tsx
+++ client-dachord/src/app/layout/App.tsx
@@ -1,36 +1,15 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import { Container } from 'semantic-ui-react';
 import HomePage from '../../features/home/HomePage';
 import EventDashboard from '../../features/musicEvents/EventDashboard';
 import EventForm from '../../features/musicEvents/form/EventForm';
-import { useStore } from '../stores/store';
-import LoadingComponent from './LoadingComponent';
+
 import NavBar from './NavBar';
 
 function App() {
-    // destructure only activityStore from the whole store
-    const { activityStore } = useStore();
-
-    useEffect(() => {
-        activityStore.loadActivities();
-      // todo (will be tested)
-     // loading icon appears during second add operation
-       // setSubmitting(false);
-
-        // notice empty dependency [] to avoid endless loop
-        // runs only one time
-}, [activityStore])
-
-    
-
-
-    
-
-    if (activityStore.loadingInitial) return <LoadingComponent content='Loading Dachord App' />
-
+  
 
   return (
       <>
--- client-dachord/src/features/musicEvents/EventDashboard.tsx
+++ client-dachord/src/features/musicEvents/EventDashboard.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Grid } from 'semantic-ui-react';
+import LoadingComponent from '../../app/layout/LoadingComponent';
 import { Activity } from '../../app/models/activity';
 import { useStore } from '../../app/stores/store';
 import EventDetails from './details/EventDetails';
@@ -13,6 +14,27 @@ export default observer( function EventDashboard() {
 
     const { activityStore } = useStore();
     const { selectedEvent, isEditable } = activityStore;
+
+    // destructure only activityStore from the whole store
+
+    useEffect(() => {
+        activityStore.loadActivities();
+        // todo (will be tested)
+        // loading icon appears during second add operation
+        // setSubmitting(false);
+
+        // notice empty dependency [] to avoid endless loop
+        // runs only one time
+    }, [activityStore])
+
+
+
+
+
+
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading Dachord App' />
+
+
     return(
         <Grid>
             <Grid.Column width="10">
