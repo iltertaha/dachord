5965c74e427883771559bbb64acb1564000a1a3a
iltertaha <ilteraktolga@gmail.com>
2022-03-20 23:50:31 +0300

    Remove unused imports

--- client-dachord/src/features/musicEvents/EventDashboard.tsx
+++ client-dachord/src/features/musicEvents/EventDashboard.tsx
@@ -2,18 +2,13 @@ import { observer } from 'mobx-react-lite';
 import React, { useEffect } from 'react';
 import {Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../app/layout/LoadingComponent';
-import { Activity } from '../../app/models/activity';
 import { useStore } from '../../app/stores/store';
-import EventDetails from './details/EventDetails';
 import EventList from './EventList';
-import EventForm from './form/EventForm';
-
 
 
 export default observer( function EventDashboard() {
 
     const { activityStore } = useStore();
-    const { selectedEvent, isEditable } = activityStore;
 
     // destructure only activityStore from the whole store
 
