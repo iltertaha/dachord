322a5fd478804814c9e26b3a566dd91dee5c096d
iltertaha <ilteraktolga@gmail.com>
2022-03-27 19:04:58 +0300

    Sidebar styling

--- client-dachord/src/features/musicEvents/dashboard/EventFilters.tsx
+++ client-dachord/src/features/musicEvents/dashboard/EventFilters.tsx
@@ -5,7 +5,7 @@ import Calendar from 'react-calendar'
 export default function EventFilters() {
     return (
         <>
-            <Menu vertical size='large' style={{width:'100%'}}>
+            <Menu vertical size='large' style={{width:'100%',marginTop: 25}}>
                 <Header icon='filter' attached color='teal' content='Filters'/>
                 <Menu.Item content='All Music Events' />
                 <Menu.Item content='Going to the event' />
