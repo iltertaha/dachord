3a415a6f63100d5e2f9b67e87783b781ae5b1177
iltertaha <ilteraktolga@gmail.com>
2022-03-27 19:32:12 +0300

    Homepage styling

--- client-dachord/src/features/home/HomePage.tsx
+++ client-dachord/src/features/home/HomePage.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import { Container, Header, Segment, Image, Button } from 'semantic-ui-react';
 
 export default function HomePage() {
     return (
-        <Container style={{marginTop: '7em'}}>
-            <h1>Home Page</h1>
-            <h3>Take me to the <Link to='/musicEvents'>Music Events</Link></h3>
-        </Container>
+        <Segment inverted textAlign="center" vertical className="intropage">
+            <Container>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
+                    D'achord
+                </Header>
+                <Header as='h2' inverted content="Welcome to D'achord!" />
+                <Button as={Link} to='/musicEvents' size='huge' inverted> Take me to the musical events</Button>
+            </Container>
+        </Segment>
         )
 }
