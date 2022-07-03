16a771d77b49e687406a5f8f3444d220e3482fc2
iltertaha <ilteraktolga@gmail.com>
2022-07-03 13:45:44 +0300

    Create User Dto

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+    }
+}
