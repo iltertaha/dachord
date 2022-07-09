26916eb47953405a176d43abc4f4632da271b61d
iltertaha <ilteraktolga@gmail.com>
2022-07-09 17:48:48 +0300

    Show different buttons if not logged in

--- client-dachord/src/app/layout/NavBar.tsx
+++ client-dachord/src/app/layout/NavBar.tsx
@@ -1,14 +1,17 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import {Button, Container, Menu } from 'semantic-ui-react';
+import { Link, NavLink } from 'react-router-dom';
+import {Button, Container, Menu, Image, Dropdown } from 'semantic-ui-react';
+import { useStore } from '../stores/store';
 
 
 
 
-export default function NavBar() {
+export default observer(function NavBar() {
 
-   
+    const{ userStore: {user,logout} } = useStore();
 
+ 
     return(
         <Menu inverted fixed='top'>
             <Container>
@@ -22,8 +25,17 @@ export default function NavBar() {
                     <Button as={NavLink} to='/createEvent' positive content="Create Music Event" />
                 </Menu.Item>
                 <Menu.Item />
+                <Menu.Item position='right'>
+                    <Image src={user?.image || 'assets/user.png' } avatar spaced="right"/>
+                    <Dropdown pointing='top left' 
+                    text={user?.displayName}>
+                        <Dropdown.Item as={Link} to={`/profile/${user?.username}`} text="My Profile" icon='user' />
+                        <Dropdown.Item onClick={logout} text="Logout" icon="power"/>
+
+                    </Dropdown>
+                </Menu.Item>
 
             </Container>
         </Menu>
     )
-}
+})
--- client-dachord/src/features/home/HomePage.tsx
+++ client-dachord/src/features/home/HomePage.tsx
@@ -1,8 +1,12 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Header, Segment, Image, Button } from 'semantic-ui-react';
+import { useStore } from '../../app/stores/store';
 
-export default function HomePage() {
+export default observer(function HomePage() {
+    const {userStore} = useStore();
+
     return (
         <Segment inverted textAlign="center" vertical className="intropage">
             <Container>
@@ -10,9 +14,21 @@ export default function HomePage() {
                     <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12, marginLeft: -50}} />
                     Dachord
                 </Header>
-                <Header as='h2' inverted content="Welcome to D'achord!" />
-                <Button as={Link} to='/login' size='huge' inverted> Login</Button>
+                
+                {userStore.isLoggedIn ? (
+                    <>
+                    <Header as='h2' inverted content="Welcome to D'achord!" />
+                    <Button as={Link} to='/activities' size='huge' inverted> Go to D'achord</Button>
+                    </>
+                    
+                ) : (
+                    // if not logged in
+                    <Button as={Link} to='/login' size='huge' inverted> Login</Button>
+                    
+                )}
+                
+                
             </Container>
         </Segment>
         )
-}
+})
