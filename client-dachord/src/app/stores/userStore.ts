158e671cd889a444b26e91e1745f52d5eec129c4
iltertaha <ilteraktolga@gmail.com>
2022-07-09 16:29:39 +0300

    Set token while login and logout

--- client-dachord/src/app/stores/userStore.ts
+++ client-dachord/src/app/stores/userStore.ts
@@ -1,6 +1,9 @@
+import { history } from './../../index';
+import { runInAction } from 'mobx';
 import { makeAutoObservable } from 'mobx';
 import agent from '../api/agent';
 import { User, UserFormValues } from "../models/user";
+import { store } from './store';
 
 export default class UserStore{
     user: User | null = null;
@@ -16,9 +19,20 @@ export default class UserStore{
     login = async (creds: UserFormValues) => {
         try{
             const user = await agent.Account.login(creds);
+             store.commonStore.setToken(user.token);
+             runInAction(() => this.user = user);
+             history.push('/activities');
+             this.user = user;
             console.log(user);
         }catch(error){
             throw error;
         }
+    }
+
+    logout = () => {
+        store.commonStore.setToken(null);
+        window.localStorage.removeItem('jwt');
+        this.user = null;
+        history.push('/');
     }
 }
