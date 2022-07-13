f26fa8dac37cf370344fb330d0a54dddef880d98
iltertaha <ilteraktolga@gmail.com>
2022-07-13 18:46:52 +0300

    Attending endpoint

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -49,9 +49,14 @@ namespace API.Controllers
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
+        }
 
-
-
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id){
+            return HandleResult(await Mediator.Send(
+                new UpdateAttendance.Command{Id = id}
+            ));
+            
         }
     }
 }
