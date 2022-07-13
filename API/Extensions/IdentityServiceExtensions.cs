8fe004c7c4665ae2443360b9ad16e1e35d6ce989
iltertaha <ilteraktolga@gmail.com>
2022-07-13 19:25:53 +0300

    Custom Policy for IsHost endpoint

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using API.Services;
 using Domain;
+using Infrastructure.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
@@ -37,6 +39,14 @@ namespace API.Extensions
 
                     };
                 });
+
+            services.AddAuthorization( opt => {
+                opt.AddPolicy("IsActivityHost", policy => {
+                    policy.Requirements.Add(new IsHostRequirement());
+
+                });
+            });
+            services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
             services.AddScoped<TokenService>();
             
             return services;
