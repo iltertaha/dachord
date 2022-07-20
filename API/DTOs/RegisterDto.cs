12ada4b80fad5f55f21c833110d4dbe49d645c1c
iltertaha <ilteraktolga@gmail.com>
2022-07-20 20:28:07 +0300

    Validation message for user registration

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,9 +1,19 @@
-﻿namespace API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        // Password should contain numbers, lowercase and uppercase chars and nonalphanumeric char
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
         public string Password { get; set; }
         public string UserName { get; set; }
     }
