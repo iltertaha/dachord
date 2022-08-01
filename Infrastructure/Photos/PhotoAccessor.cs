8e1346e2f21510fe69941a888037937ee00f5c6e
iltertaha <ilteraktolga@gmail.com>
2022-08-01 20:07:41 +0300

    Implement Add and Delete Photo Methods

--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Interfaces;
 using Application.Photos;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +14,50 @@ namespace Infrastructure.Photos
 {
     public class PhotoAccessor : IPhotoAccessor
     {
-        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        private readonly Cloudinary _cloudinary;
+        public PhotoAccessor(IOptions<CloudinaryConfigs> config) 
         {
-            throw new NotImplementedException();
+            var cloudinaryAccount = new Account(
+                config.Value.CloudName,config.Value.ApiKey,config.Value.ApiSecret);
+            
+            _cloudinary = new Cloudinary(cloudinaryAccount);
         }
 
-        public Task<string> DeletePhoto(string publicId)
+        public async Task<PhotoUploadResult> AddPhoto(IFormFile file)
         {
-            throw new NotImplementedException();
+            if(file.Length > 0)
+            {
+                // use "using" keyword to dispose the resource after use
+                await using var fileStream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, fileStream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill")
+                };
+
+                var uploadImageResult = await _cloudinary.UploadAsync(uploadParams);
+
+                if(uploadImageResult != null)
+                {
+                    throw new Exception(uploadImageResult.Error.Message);
+                }
+
+                // happy path
+                return new PhotoUploadResult
+                {
+                    PublicId = uploadImageResult.PublicId,
+                    Url = uploadImageResult.SecureUrl.ToString()
+                };
+            }
+            return null;
+        }
+
+        public async Task<string> DeletePhoto(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+            var deletionResult = await _cloudinary.DestroyAsync(deleteParams);
+
+            return deletionResult.Result == "ok" ? deletionResult.Result : null;
         }
     }
 }
