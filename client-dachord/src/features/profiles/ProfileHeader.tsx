957a752d22b286f3da86d956a0ea926f9014e004
iltertaha <ilteraktolga@gmail.com>
2022-08-03 22:14:51 +0300

    Show photos of the user in the profile

--- client-dachord/src/features/profiles/ProfileHeader.tsx
+++ client-dachord/src/features/profiles/ProfileHeader.tsx
@@ -1,7 +1,15 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Button, Divider, Grid, Header, Item, Reveal, Segment, Statistic } from 'semantic-ui-react';
+import { Profile } from '../../app/models/profile';
 
-export default function ProfileHeader() {
+
+interface Props {
+    profile: Profile;
+}
+
+// need to make it observer even though we dont use store here
+export default observer(function ProfileHeader({profile} : Props) {
 
     return (
         <Segment>
@@ -9,9 +17,9 @@ export default function ProfileHeader() {
                 <Grid.Column width={12}>
                     <Item.Group>
                         <Item>
-                            <Item.Image avatar size='small' src={'/assets/user.png'} />
+                            <Item.Image avatar size='small' src={profile.image || '/assets/user.png'} />
                             <Item.Content verticalAlign='middle'>
-                                <Header as='h1' content='Displayname' />
+                                <Header as='h1' content={profile.displayName} />
                             </Item.Content>
                         </Item>
                     </Item.Group>
@@ -34,4 +42,4 @@ export default function ProfileHeader() {
             </Grid>
         </Segment>
     )
-}
+})
