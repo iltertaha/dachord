e5e371158fad03f7eb0c156982418997c86a0879
iltertaha <ilteraktolga@gmail.com>
2022-08-06 21:14:42 +0300

    List comments endpoint

---
+++ Application/Comments/List.cs
@@ -0,0 +1,45 @@
+﻿using Application.core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Comments
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<CommentDto>>>
+        {
+            public Guid ActivityId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<CommentDto>>>
+        {
+            private readonly DataContext context;
+            private readonly IMapper mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                this.context = context;
+                this.mapper = mapper;
+            }
+
+            public async Task<Result<List<CommentDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var comments = await context.Comments
+                    .Where(x => x.Activity.Id == request.ActivityId)
+                    .OrderBy(x => x.CreatedAt)
+                    .ProjectTo<CommentDto>(mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<CommentDto>>.Success(comments);
+            }
+
+        }
+    }
+}
